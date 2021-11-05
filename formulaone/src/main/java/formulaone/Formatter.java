485d417f25bb7a0b7fcd8ab7146a9d3d8101d2db
Evgeny Sabarov <e.sabarov@gmail.com>
2021-11-05 18:06:28 +0300

    changed formatRacersResult method

--- formulaone/src/main/java/formulaone/Formatter.java
+++ formulaone/src/main/java/formulaone/Formatter.java
@@ -3,20 +3,31 @@ package formulaone;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Formatter {
     
-    
+    RacerParser racerParser;
+    Racer racer;
     private static final DateTimeFormatter FORMATTER_TIME = DateTimeFormatter.ofPattern("mm:ss.SSS");
     private static final String SPACE = " ";
     private static final String DASH = "-";
     private static final String POINT = ".";
     private static final String NEW_LINE = "\n";
     private static final String EQUAL = "=";
-     
-    public String formatRacersResults(Map<String, Long> resultRacers, RacerParser racerParser) {
+    private static final String VERTICAL_LINE = "|";
+    
+    public String formatRacersResult(String fileNameAndTeam, String fileStartTime, String fileEndTime,
+            RacerParser racerParser, FileReader fileReader) {
+        Map<String, Racer> listRacers = racerParser.initializationRacers(fileNameAndTeam, fileStartTime, fileEndTime, fileReader);
+        Map<String, Long> resultRacers = new HashMap<>();
+        for (Map.Entry<String, Racer> entry : listRacers.entrySet()) {
+            String key = entry.getKey();
+            Racer value = entry.getValue();
+            resultRacers.put(key, value.resultTime);
+        }
         List<String[]> list = new ArrayList();
         resultRacers.entrySet().stream()
         .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
@@ -25,7 +36,12 @@ public class Formatter {
         StringBuilder sb = new StringBuilder();
         for(int j = list.size() - 1; j > - 1; j--) {
             sb.append(list.size() - j).append(POINT).append((j > 9 ? spaces(2) : spaces(1)))
-                    .append(racerParser.getRasers().get(list.get(j)[0]))
+                    .append(listRacers.get(list.get(j)[0]).fullName)
+                    .append(spaces(17 - listRacers.get(list.get(j)[0]).fullName.length()))
+                    .append(SPACE).append(VERTICAL_LINE).append(SPACE)
+                    .append(listRacers.get(list.get(j)[0]).team)
+                    .append(spaces(26 - listRacers.get(list.get(j)[0]).team.length()))
+                    .append(VERTICAL_LINE).append(SPACE)
                     .append(LocalTime.ofNanoOfDay(resultRacers.get(list.get(j)[0]))
                     .format(FORMATTER_TIME)).append(NEW_LINE);
             if(j == 4) {
@@ -42,6 +58,7 @@ public class Formatter {
         }
         return sb.toString();
     }
+    
     private String dashs(int value) {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < value; i++) {
@@ -49,6 +66,5 @@ public class Formatter {
         }
         return sb.toString();
     }
-
-
+    
 }
