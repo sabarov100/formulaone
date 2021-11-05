e154efea8ebfc6a74d8922d19c9e3d0e28d94e6d
Evgeny Sabarov <e.sabarov@gmail.com>
2021-11-05 18:04:11 +0300

    changed class to only read

--- formulaone/src/main/java/formulaone/FileReader.java
+++ formulaone/src/main/java/formulaone/FileReader.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,9 +13,7 @@ import java.util.stream.Stream;
 public class FileReader {
 
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS");
-    private static final String SPACE = " ";
     private static final String UNDERLINE = "_";
-    private static final String VERTICAL_LINE = "|";
     
     public Map<String, LocalDateTime> getData(String fileName) {
         try {
@@ -24,29 +23,18 @@ public class FileReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return Collections.emptyMap();
     }
     
-    public Map<String, String> getDataRacers(String fileName) {
+    public Map<String, String[]> getDataRacersAndTeams(String fileName) {
         try {
             Stream<String> streamFromFiles = Files.lines(Paths.get(fileName));
             return streamFromFiles
-                    .collect(Collectors.toMap(s -> s.substring(0, 3), s ->  s.substring(4, s.length()).split(UNDERLINE)[0]
-                            + spaces(17 - s.substring(4, s.length()).split(UNDERLINE)[0].length()) + SPACE + VERTICAL_LINE 
-                            + SPACE + s.substring(4, s.length()).split(UNDERLINE)[1]
-                            + spaces(26 - s.substring(4, s.length()).split(UNDERLINE)[1].length()) + VERTICAL_LINE + SPACE));
+                    .collect(Collectors.toMap(s -> s.substring(0, 3), s ->  s.substring(4, s.length()).split(UNDERLINE)));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
-    }
-    
-    private String spaces(int value) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < value; i++) {
-            sb.append(SPACE);
-        }
-        return sb.toString();
+        return Collections.emptyMap();
     }
 
 }
