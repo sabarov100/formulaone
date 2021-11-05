fc9ccb651ef3293536b304bc996ba42b53697780
Evgeny Sabarov <e.sabarov@gmail.com>
2021-11-05 18:08:57 +0300

    deleted RacerData from constructor

--- formulaone/src/main/java/formulaone/Main.java
+++ formulaone/src/main/java/formulaone/Main.java
@@ -3,7 +3,7 @@ package formulaone;
 public class Main {
     
     public static void main(String[] args) {
-        Facade facade = new Facade(new FileReader(), new RacersData(), new RacerParser(), new Formatter());
+        Facade facade = new Facade(new FileReader(), new RacerParser(), new Formatter());
         String fileNameStartTime = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\start.log";
         String fileNameEndTime = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\end.log";
         String fileNameAbbrevAndFullnameAndTeamOfRacers = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\abbreviations.txt";
