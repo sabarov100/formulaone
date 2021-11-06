f12643a3fc48c83629bd16cb997dab7431f9608e
Evgeny Sabarov <e.sabarov@gmail.com>
2021-11-06 23:13:27 +0300

    changed fields

--- formulaone/src/main/java/formulaone/Main.java
+++ formulaone/src/main/java/formulaone/Main.java
@@ -2,12 +2,15 @@ package formulaone;
 
 public class Main {
     
+    public static String fileNameStartTime = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\start.log";
+    public static String fileNameEndTime = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\end.log";
+    public static String fileNameRacersInfo = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\abbreviations.txt";
+   
     public static void main(String[] args) {
+        
         Facade facade = new Facade(new FileReader(), new RacerParser(), new Formatter());
-        String fileNameStartTime = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\start.log";
-        String fileNameEndTime = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\end.log";
-        String fileNameAbbrevAndFullnameAndTeamOfRacers = "C:\\repositoryfour\\formulaone\\src\\main\\resources\\abbreviations.txt";
-        String result = facade.bestRacersResultsOfRing(fileNameStartTime, fileNameEndTime, fileNameAbbrevAndFullnameAndTeamOfRacers);
+        String result = facade.bestRacersResultsOfRing(fileNameStartTime, fileNameEndTime, fileNameRacersInfo);
         System.out.println(result);
+        
     }
 }
